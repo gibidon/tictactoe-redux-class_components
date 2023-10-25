1c01045eaea21e04a9a027e0fb6360e1187f97d1
gibidon <malykhinig@gmail.com>
2023-10-25 12:17:02 +0700

    v2 trash

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -1,22 +1,63 @@
 import { Component } from "react"
 import { Cell } from "./components/Cell"
 import { connect } from "react-redux"
+import { PLAYER, PLAYER_SIGN } from "../../constants"
+import { fillCell } from "../../actions/fillCell"
+import { checkWin, checkEmptyCell } from "../../utils"
 
 export class FieldContainer extends Component {
+	// constructor(props) {
+	// 	super(props)
+	// 	this.handleClick = props.handleClick
+	// 	this.winner = props.winner
+	// 	this.player = props.player
+	// 	this.status = props.status
+	// 	console.log(this.status)
+
+	// }
+	// handleClick(index) {
+	// 	if (
+	// 		this.status === "WIN" ||
+	// 		this.status === "DRAW" ||
+	// 		this.content !== PLAYER.NOBODY
+	// 	) {
+	// 		return
+	// 	}
+	// 	dispatch(fillCell(index))
+	// 	const newField = [...this.field]
+	// 	newField[index] = this.player
+
+	// 	if (checkWin(newField, this.player)) {
+	// 		dispatch({ type: "SET_WINNER", payload: this.player })
+	// 		dispatch({ type: "SET_STATUS", payload: "WIN" })
+	// 		return
+	// 	} else if (!checkEmptyCell(newField)) {
+	// 		dispatch({ type: "SET_STATUS", payload: "DRAW" })
+	// 	}
+	// }
 	render() {
+		console.log("rendering field...")
 		return (
 			<div className="container mx-auto md p-10">
 				<div className="grid grid-cols-3 gap-2 p-10 bg-slate-50 shadow-lg max-w-sm rounded-lg mx-auto">
-					{/* {this.field.map((cell, index) => ( */}
 					{this.props.field.map((cell, index) => (
-						<Cell
+						<button
 							content={cell}
-							key={index}
 							index={index}
-							winner={this.props.winner}
-							status={this.props.status}
-							player={this.props.player}
-						/>
+							key={index}
+							className="w-20 h-20 bg-slate-200 rounded-md hover:bg-slate-400"
+							onClick={() => {
+								// console.log(this.player)
+								// console.log(this.props.player)
+								this.props.handleClick(index, this.props)
+							}}
+							// onClick={() => {
+
+							// }
+							// onClick={() => this.handleClick(index)}
+						>
+							{PLAYER_SIGN[cell]}
+						</button>
 					))}
 				</div>
 			</div>
@@ -29,5 +70,36 @@ const mapStateToProps = (state) => ({
 	player: state.game.player,
 	status: state.game.status,
 	winner: state.game.winner,
+	// {field,player,status,winner} = state.game
 })
-export const Field = connect(mapStateToProps)(FieldContainer)
+const mapDispatchToProps = (dispatch) => ({
+	handleClick: (index, props) => {
+		// console.log(this.props.player)
+		console.log("handleClick mock..", index)
+		console.log(props)
+		// dispatch(fillCell(index))
+		if (
+			props.status === "WIN" ||
+			props.status === "DRAW"
+			// this.content !== PLAYER.NOBODY
+		) {
+			return
+		}
+		dispatch(fillCell(index))
+		const newField = [...props.field]
+		newField[index] = props.player
+
+		if (checkWin(newField, props.player)) {
+			dispatch({ type: "SET_WINNER", payload: props.player })
+			dispatch({ type: "SET_STATUS", payload: "WIN" })
+			return
+		} else if (!checkEmptyCell(newField)) {
+			dispatch({ type: "SET_STATUS", payload: "DRAW" })
+		}
+	},
+})
+
+export const Field = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(FieldContainer)
--- src/components/Field/components/Cell.jsx
+++ src/components/Field/components/Cell.jsx
@@ -5,19 +5,18 @@ import { connect } from "react-redux"
 import { checkWin, checkEmptyCell } from "../../../utils"
 
 export class CellContainer extends Component {
-	constructor({ index, player, winner, status }) {
-		super()
+	constructor({ index }) {
+		super(index)
 		// this.content = content
 		this.index = index
 	}
 
 	render() {
-		console.log(this.props)
 		return (
 			<button
 				className="h-20 w-20 bg-slate-200 rounded-md hover:bg-slate-300"
-				onClick={() => this.props.handleClick(this.index)}
-				// onClick={() => this.props.handleClick2(this.index)}
+				// onClick={() => this.props.handleClick(this.index)}
+				onClick={() => this.props.handleClick2(this.index)}
 			>
 				<span className="font-medium text-2xl">
 					{PLAYER_SIGN[this.props.content]}
@@ -31,25 +30,25 @@ const mapDispatchToProps = (dispatch) => ({
 	handleClick: (index) => {
 		dispatch(fillCell(index))
 	},
-	// handleClick2: (index) => {
-	// 	if (
-	// 		this.props.status === "WIN" ||
-	// 		this.props.status === "DRAW" ||
-	// 		this.content !== PLAYER.NOBODY
-	// 	) {
-	// 		return
-	// 	}
-	// 	dispatch(fillCell(index))
-	// 	const newField = [...this.props.field]
-	// 	newField[index] = this.props.player
+	handleClick2: (index) => {
+		if (
+			this.status === "WIN" ||
+			this.status === "DRAW" ||
+			this.content !== PLAYER.NOBODY
+		) {
+			return
+		}
+		dispatch(fillCell(index))
+		const newField = [...this.field]
+		newField[index] = this.player
 
-	// 	if (checkWin(newField, this.props.player)) {
-	// 		dispatch({ type: "SET_WINNER", payload: this.props.player })
-	// 		dispatch({ type: "SET_STATUS", payload: "WIN" })
-	// 		return
-	// 	} else if (!checkEmptyCell(newField)) {
-	// 		dispatch({ type: "SET_STATUS", payload: "DRAW" })
-	// 	}
-	// },
+		if (checkWin(newField, this.player)) {
+			dispatch({ type: "SET_WINNER", payload: this.player })
+			dispatch({ type: "SET_STATUS", payload: "WIN" })
+			return
+		} else if (!checkEmptyCell(newField)) {
+			dispatch({ type: "SET_STATUS", payload: "DRAW" })
+		}
+	},
 })
 export const Cell = connect(null, mapDispatchToProps)(CellContainer)
