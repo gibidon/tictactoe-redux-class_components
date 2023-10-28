634f6731c1fecbd798b19d36d7e1748bc62acf3e
gibidon <malykhinig@gmail.com>
2023-10-28 17:15:34 +0700

    v8 before FieldLayout

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -2,13 +2,11 @@ import { Component } from "react"
 import { connect } from "react-redux"
 import { PLAYER, PLAYER_SIGN } from "../../constants"
 import { checkWin, checkEmptyCell } from "../../utils"
-// import { FieldLayout } from "./FieldLayout"
 
 export class FieldContainer extends Component {
-  handleCLick(index, content) {
+  handleCLick(index) {
     let { player, status, field, dispatch } = this.props
-    // console.log("handleClick", player, status, field, dispatch)
-    if (status === "WIN" || status === "DRAW" || content !== PLAYER.NOBODY) {
+    if (status === "WIN" || status === "DRAW" || field[index] !== PLAYER.NOBODY) {
       return
     }
     dispatch({ type: "FILL_CELL", payload: index })
@@ -23,12 +21,10 @@ export class FieldContainer extends Component {
       dispatch({ type: "SET_STATUS", payload: "DRAW" })
     }
   }
+
   render() {
-    let { player, status, field, dispatch } = this.props
-    // console.log(this.handleCLick)
+    let { field, dispatch } = this.props
     return (
-      // console.log(this.handleCLick)
-      // <FieldLayout field={field} handleClick={this.handleClick} />
       <div className="container mx-auto md p-10">
         <div className="grid grid-cols-3 gap-2 p-10 bg-slate-200 shadow-xl max-w-sm rounded-lg mx-auto">
           {field.map((cellPlayer, index) => (
@@ -37,7 +33,7 @@ export class FieldContainer extends Component {
               key={index}
               className="w-20 h-20 bg-slate-300 rounded-md hover:bg-slate-400 font-medium text-xl group relative overflow-hidden"
               onClick={() => {
-                this.handleCLick(index, cellPlayer)
+                this.handleCLick(index)
               }}
             >
               {PLAYER_SIGN[cellPlayer]}
